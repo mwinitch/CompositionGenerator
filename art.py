10153273c817230f786bd0fe21d5b1e97ecf515c
mwinitch <michaelwin26@gmail.com>
2021-03-03 19:32:25 -0500

    Initial commit

---
+++ art.py
@@ -0,0 +1,201 @@
+import turtle
+from turtle import Screen
+import random
+from random import randint, randrange
+import socket
+import threading
+
+UDP_IP = "" # The IP that is printed in the serial monitor from the ESP32
+SHARED_UDP_PORT = 4210
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
+sock.connect((UDP_IP, SHARED_UDP_PORT))
+screen = Screen()
+screen.setup(2000, 1000)
+turtle.title("Creative Random Art")
+
+LEFT = -800
+RIGHT = 800
+TOP = 400
+BOTTOM = -400
+horizontals = [BOTTOM, TOP]
+verticals = [LEFT, RIGHT]
+colors = ["red", "yellow", "blue", "white"]
+btn1 = "1"
+color = random.choice(colors)
+
+def x_pos():
+    return randint(LEFT, RIGHT)
+
+def y_pos():
+    return randint(BOTTOM, TOP)
+
+t = turtle.Turtle()
+
+def down_up_right():
+    global t, verticals, horizontals, color
+    t.color("black", color)
+    t.begin_fill()
+    start_x = x_pos()
+    verticals.append(start_x)
+    verticals.sort()
+    vertical_index = verticals.index(start_x)
+
+    start_index = randint(0, len(horizontals) - 2)
+    start_y = horizontals[start_index]
+    t.hideturtle()           
+    t.penup()                
+    t.goto(start_x, start_y)
+    t.showturtle()           
+    t.pendown()
+    end_index = randint(start_index + 1, len(horizontals) - 1)
+    end_y = horizontals[end_index]
+    t.goto(start_x, end_y)
+
+
+    if verticals[vertical_index + 1] == RIGHT:
+        top_right = RIGHT
+    else:
+        idx = randint(vertical_index + 1, len(verticals) - 1)
+        top_right = verticals[idx]
+    
+    t.goto(top_right, end_y)
+    t.goto(top_right, start_y)
+    t.goto(start_x, start_y)
+    t.end_fill()
+
+
+def down_up_left():
+    global t, verticals, horizontals, color
+    t.color("black", color)
+    t.begin_fill()
+    start_x = x_pos()
+    verticals.append(start_x)
+    verticals.sort()
+    vertical_index = verticals.index(start_x)
+
+    start_index = randint(0, len(horizontals) - 2)
+    start_y = horizontals[start_index]
+    t.hideturtle()           
+    t.penup()                
+    t.goto(start_x, start_y)
+    t.showturtle()           
+    t.pendown()
+    end_index = randint(start_index + 1, len(horizontals) - 1)
+    end_y = horizontals[end_index]
+    t.goto(start_x, end_y)
+
+
+    if verticals[vertical_index - 1] == LEFT:
+        top_right = LEFT
+    else:
+        idx = randint(0, vertical_index - 1)
+        top_right = verticals[idx]
+    
+    t.goto(top_right, end_y)
+    t.goto(top_right, start_y)
+    t.goto(start_x, start_y)
+    t.end_fill()
+
+
+def left_right_up():
+    global t, verticals, horizontals, color
+    t.color("black", color)
+    t.begin_fill()
+    start_y = y_pos()
+    horizontals.append(start_y)
+    horizontals.sort()
+    horizontal_index = horizontals.index(start_y)
+
+    start_index = randint(0, len(verticals) - 2)
+    start_x = verticals[start_index]
+    t.hideturtle()           
+    t.penup()                
+    t.goto(start_x, start_y)
+    t.showturtle()           
+    t.pendown()
+    
+    end_index = randint(start_index + 1, len(verticals) - 1)
+    end_x = verticals[end_index]
+    t.goto(end_x, start_y)
+
+    if horizontals[horizontal_index+ 1] == TOP:
+        top_right = TOP
+    else:
+        idx = randint(horizontal_index + 1, len(horizontals) - 1)
+        top_right = horizontals[idx]
+    
+    t.goto(end_x, top_right)
+    t.goto(start_x, top_right)
+    t.goto(start_x, start_y)
+    t.end_fill()
+
+
+def left_right_down():
+    global t, verticals, horizontals, color
+    t.color("black", color)
+    t.begin_fill()
+    start_y = y_pos()
+    horizontals.append(start_y)
+    horizontals.sort()
+    horizontal_index = horizontals.index(start_y)
+
+    start_index = randint(0, len(verticals) - 2)
+    start_x = verticals[start_index]
+    t.hideturtle()           
+    t.penup()                
+    t.goto(start_x, start_y)
+    t.showturtle()           
+    t.pendown()
+    
+    end_index = randint(start_index + 1, len(verticals) - 1)
+    end_x = verticals[end_index]
+    t.goto(end_x, start_y)
+
+    if horizontals[horizontal_index - 1] == BOTTOM:
+        top_right = BOTTOM
+    else:
+        idx = randint(0, horizontal_index - 1)
+        top_right = horizontals[idx]
+    
+    t.goto(end_x, top_right)
+    t.goto(start_x, top_right)
+    t.goto(start_x, start_y)
+    t.end_fill()
+
+moves = [left_right_down, down_up_left, left_right_up, down_up_right]
+move = random.choice(moves)
+
+# Listens to the UDP socket for all incoming data
+def collecting():
+    global color, moves, move
+    while True:
+        data = sock.recv(2048).decode()
+        print(data)
+        if data[0] == '0':
+            color = random.choice(colors)
+        if data[1] == '0':
+            move = random.choice(moves)
+
+sock.send('Hello ESP32'.encode())
+thread = threading.Thread(target=collecting)
+thread.daemon = True
+thread.start()
+
+
+# Creating Iniital Frame
+t.speed(6)
+t.hideturtle()           
+t.penup()                
+t.goto(LEFT, TOP)
+t.showturtle()           
+t.pendown()
+t.width(3)
+t.goto(RIGHT, TOP)
+t.goto(RIGHT, BOTTOM)
+t.goto(LEFT, BOTTOM)
+t.goto(LEFT, TOP)
+
+while True:
+    move()   
+
+turtle.done() 
---
+++ input/input.ino
@@ -0,0 +1,63 @@
+
+#include <WebServer.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+//set up to connect to an existing network (e.g. mobile hotspot from laptop that will run the python code)
+const char* ssid = "";
+const char* password = "";
+WiFiUDP Udp;
+unsigned int localUdpPort = 4210;  //  port to listen on
+char incomingPacket[255];  // buffer for incoming packets
+
+void setup()
+{
+  int status = WL_IDLE_STATUS;
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  pinMode(18, INPUT_PULLUP);
+  pinMode(32, INPUT_PULLUP);
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to wifi");
+  Udp.begin(localUdpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+
+  // we recv one packet from the remote so we can know its IP and port
+  bool readPacket = false;
+  while (!readPacket) {
+    int packetSize = Udp.parsePacket();
+    if (packetSize)
+     {
+      // receive incoming UDP packets
+      Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+      int len = Udp.read(incomingPacket, 255);
+      if (len > 0)
+      {
+        incomingPacket[len] = 0;
+      }
+      Serial.printf("UDP packet contents: %s\n", incomingPacket);
+      readPacket = true;
+    } 
+  }
+}
+
+void loop()
+{
+  // once we know where we got the inital packet from, send data back to that IP address and port
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+  int colorButton = digitalRead(18);
+  int directionButton = digitalRead(32);
+  Serial.print(colorButton);
+  Serial.print(directionButton);
+  Serial.print("\n");
+  Udp.printf("%d", colorButton);
+  Udp.printf("%d", directionButton);
+  Udp.endPacket();
+  delay(1000);
+}
